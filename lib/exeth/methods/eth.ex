ab8d556d2e8a22e1a392f7d619f8d0fa3a29cabd
chef <yfj211@gmail.com>
2017-06-02 11:29:51 -0400

    more methods

--- lib/exeth/methods/eth.ex
+++ lib/exeth/methods/eth.ex
@@ -1,19 +1,19 @@
 defmodule Exeth.Eth do
 	use Exeth.Macros, namespace: "eth"
 
-	rpc protocolVersion(), returns: :quantity
+	rpc protocolVersion(), returns: :string
 
-	rpc syncing()
+	rpc syncing(), returns: :object_or_boolean
 
-	rpc coinbase()
+	rpc coinbase(), returns: :string
 
-	rpc mining()
+	rpc mining(), returns: :boolean
 
 	rpc hashrate(), returns: :quantity
 
 	rpc gasPrice(), returns: :quantity
 
-	rpc account()
+	rpc accounts(), returns: :list
 
 	rpc blockNumber(), returns: :quantity
 
@@ -30,4 +30,58 @@ defmodule Exeth.Eth do
 	rpc getUncleCountByBlockHash(block), returns: :quantity
 
 	rpc getCode(address, tag), returns: :quantity
+
+	rpc sign
+
+	rpc sendTransaction
+
+	rpc sendRawTransaction
+
+	rpc call
+
+	rpc estimateGas
+
+	rpc getBlockByHash
+
+	rpc getBlockByNumber
+
+	rpc getTransactionByHash
+
+	rpc getTransactionByBlockHashAndIndex
+
+	rpc getTransactionByBlockNumberAndIndex
+
+	rpc getTransactionReceipt
+
+	rpc getUncleByBlockHashAndIndex
+
+	rpc getUncleByBlockNumberAndIndex
+
+	rpc getCompilers
+
+	rpc compileLLL
+
+	rpc compileSolidity
+
+	rpc compileSerpent
+
+	rpc newFilter
+
+	rpc newBlockFilter
+
+	rpc newPendingTransactionFilter
+
+	rpc uninstallFilter
+
+	rpc getFilterChanges
+
+	rpc getFilterLogs
+
+	rpc getLogs
+
+	rpc getWork
+
+	rpc submitWork(data), returns: :boolean
+
+	rpc submitHashrate(hashrate), returns: :boolean
 end
